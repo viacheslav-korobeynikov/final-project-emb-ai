48bb0a1c84410b0a710dabd76d08ad82fc955965
viacheslav-korobeynikov <korobeynikovvo@gmail.com>
2026-03-17 11:56:30 -0400

    Add Error Handling

--- server.py
+++ server.py
@@ -13,6 +13,8 @@ def sent_detector():
     joy = resp['joy']
     sadness = resp['sadness']
     dominant_emotion = resp['dominant_emotion']
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
     return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}"
 
 
