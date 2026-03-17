ccac0b3666ae83897a6f2b35ef678bfddeb2a86d
viacheslav-korobeynikov <korobeynikovvo@gmail.com>
2026-03-17 11:07:52 -0400

    Create package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    my_obj = {'raw_document': {'text': text_to_analyze}}
+    header = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
+    resp = requests.post(url, json=my_obj, headers=header)
+    formatted_response = json.loads(resp.text)
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    return {'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': dominant_emotion}
